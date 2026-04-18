01332aa0890858af234c5a3b2166565434a1e8f0
Leo <leoh0987@gmail.com>
2026-04-17 22:48:25 -0500

    rework project structure

--- src/eccomerce_site/urls.py
+++ src/eccomerce_site/urls.py
@@ -17,10 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
-
+from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("accounts/", include("allauth.urls")),
+    path('store/',TemplateView.as_view(template_name="store.html"), name='store'),
     path("", TemplateView.as_view(template_name="home.html"), name='home'),
     
 ]
