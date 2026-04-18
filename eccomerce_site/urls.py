2850285680415733d1ec01baf33904396b964189
Leo <leoh0987@gmail.com>
2026-04-17 22:04:08 -0500

    add back to home page button

--- eccomerce_site/urls.py
+++ eccomerce_site/urls.py
@@ -21,6 +21,6 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("accounts/", include("allauth.urls")),
-    path("", TemplateView.as_view(template_name="home.html")),
+    path("", TemplateView.as_view(template_name="home.html"), name='home'),
     
 ]
