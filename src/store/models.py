07176eb4f354f04bff77526040a627fd253bf8e4
Leo <leoh0987@gmail.com>
2026-04-25 17:12:14 -0500

    Add Logged out redirects

--- src/store/models.py
+++ src/store/models.py
@@ -1,10 +1,31 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
 class Item(models.Model):
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=150)
+    price = models.DecimalField(decimal_places=2,max_digits=12)
     date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return [self.name, self.description, self.date]
+    
+class Cart(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, 
+        on_delete=models.CASCADE, 
+        null=False, 
+        blank=True
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Cart {self.id} for {self.user}"
+    
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, related_name='items', on_delete=models.CASCADE)
+    product = models.ForeignKey(Item, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.product.name}"
--- src/store/views.py
+++ src/store/views.py
@@ -1,8 +1,42 @@
 from django.shortcuts import render
-
-# Create your views here.
-from .models import Item
+from django.shortcuts import get_object_or_404, redirect
+from .models import Item, Cart, CartItem
 
 def store(request):
-    items = Item.objects.all()
-    return render(request, 'store/store.html', {'items': items})
+
+    if request.user.is_authenticated:
+        items = Item.objects.all()
+        return render(request, 'store/store.html', {'items': items})
+    return render(request, 'home.html')
+
+
+
+
+# def add_to_cart(request, item_id):
+#     product = get_object_or_404(Item, id=item_id)
+    
+
+#     if request.user.is_authenticated:
+#         cart, created = Cart.objects.get_or_create(user=request.user)
+#     else:
+#         cart_id = request.session.get('cart_id')
+#         if cart_id:
+#             cart = Cart.objects.filter(id=cart_id, user=None).first()
+#             if not cart: 
+#                 cart = Cart.objects.create(user=None)
+#         else:
+#             cart = Cart.objects.create(user=None)
+#             request.session['cart_id'] = cart.id
+
+
+#     cart_item, created = CartItem.objects.get_or_create(
+#         cart=cart, 
+#         product=product,
+#         defaults={'price': product.price}
+#     )
+    
+#     if not created:
+#         cart_item.quantity += 1
+#         cart_item.save()
+
+#     return redirect('cart_detail')
